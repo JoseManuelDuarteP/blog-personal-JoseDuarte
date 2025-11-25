6fcc292bcfaf7d7715c7d2fc083020892a025173
Jose <josduaper@alu.edu.gva.es>
2025-11-25 09:44:26 +0000

    initial commit

---
+++ src/Controller/PageController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PageController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+
+    #[Route('/blog', name: 'blog')]
+    public function blog(): Response
+    {
+        return $this->render('page/blog.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+}
